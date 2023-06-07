0ef1ecec8fb244bffda371d34860239e5712a47f
samu.sar <samuil.sarandev@gmail.com>
2023-06-07 18:31:26 +0300

    Fix(#6): fix KeyboardAwareScrollView bug

--- dare2fit-companion-app/tabs/Activity/Activity.tsx
+++ dare2fit-companion-app/tabs/Activity/Activity.tsx
@@ -1,5 +1,5 @@
 import { FlatList, VStack, useColorModeValue } from "native-base";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { KeyboardAwareFlatList } from "react-native-keyboard-aware-scroll-view";
 
 import { COLOR_BRAND_DARK, COLOR_BRAND_LIGHT } from "../../common/constants";
 import ActivityLogger from "../../components/Activity/ActivityLogger/ActivityLogger";
@@ -8,20 +8,16 @@ const Activity = () => {
   const background = useColorModeValue(COLOR_BRAND_LIGHT, COLOR_BRAND_DARK);
 
   return (
-    <KeyboardAwareScrollView
+    <KeyboardAwareFlatList
       contentContainerStyle={{ flex: 1 }}
       style={{ backgroundColor: background }}
-    >
-      <FlatList
-        w="100%"
-        data={[0]}
-        renderItem={() => (
-          <VStack w="100%" h="100%" p={2}>
-            <ActivityLogger />
-          </VStack>
-        )}
-      />
-    </KeyboardAwareScrollView>
+      data={[0]}
+      renderItem={() => (
+        <VStack w="100%" h="100%" p={2}>
+          <ActivityLogger />
+        </VStack>
+      )}
+    />
   );
 };
 
