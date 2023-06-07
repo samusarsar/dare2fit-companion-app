417063e01465e35eaa44d90aa71752f7a92ce637
samu.sar <samuil.sarandev@gmail.com>
2023-06-07 17:50:58 +0300

    Fix(#6): fix Activity tab keyboard-blocking input bug

--- dare2fit-companion-app/tabs/Activity/Activity.tsx
+++ dare2fit-companion-app/tabs/Activity/Activity.tsx
@@ -1,21 +1,27 @@
-import { FlatList, ScrollView, VStack, useColorModeValue } from "native-base";
+import { FlatList, VStack, useColorModeValue } from "native-base";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
+import { COLOR_BRAND_DARK, COLOR_BRAND_LIGHT } from "../../common/constants";
 import ActivityLogger from "../../components/Activity/ActivityLogger/ActivityLogger";
 
 const Activity = () => {
-  const background = useColorModeValue("brand.light", "brand.dark");
+  const background = useColorModeValue(COLOR_BRAND_LIGHT, COLOR_BRAND_DARK);
 
   return (
-    <FlatList
-      w="100%"
-      bg={background}
-      data={[0]}
-      renderItem={() => (
-        <VStack w="100%" h="100%" p={2}>
-          <ActivityLogger />
-        </VStack>
-      )}
-    />
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ flex: 1 }}
+      style={{ backgroundColor: background }}
+    >
+      <FlatList
+        w="100%"
+        data={[0]}
+        renderItem={() => (
+          <VStack w="100%" h="100%" p={2}>
+            <ActivityLogger />
+          </VStack>
+        )}
+      />
+    </KeyboardAwareScrollView>
   );
 };
 
