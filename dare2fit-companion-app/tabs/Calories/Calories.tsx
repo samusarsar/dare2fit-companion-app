ab076e96cecc5400b662109b09e1de3b02687fa1
Simona Ilcheva <sdmileva@gmail.com>
2023-06-07 23:55:17 +0300

    Chore(#7): fix KeyboardAwareScrollView bug

--- dare2fit-companion-app/tabs/Calories/Calories.tsx
+++ dare2fit-companion-app/tabs/Calories/Calories.tsx
@@ -1,5 +1,5 @@
-import { FlatList, VStack, useColorModeValue } from "native-base";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { VStack, useColorModeValue } from "native-base";
+import { KeyboardAwareFlatList } from "react-native-keyboard-aware-scroll-view";
 
 import CalorieLogger from "../../components/Calories/CalorieLogger/CalorieLogger";
 
@@ -7,20 +7,16 @@ const Calories = () => {
   const background = useColorModeValue("brand.light", "brand.dark");
 
   return (
-    <KeyboardAwareScrollView
+    <KeyboardAwareFlatList
       contentContainerStyle={{ flex: 1 }}
       style={{ backgroundColor: background }}
-    >
-      <FlatList
-        w="100%"
-        data={[0]}
-        renderItem={() => (
-          <VStack w="100%" h="100%" p={2}>
-            <CalorieLogger />
-          </VStack>
-        )}
-      />
-    </KeyboardAwareScrollView>
+      data={[0]}
+      renderItem={() => (
+        <VStack w="100%" h="100%" p={2}>
+          <CalorieLogger />
+        </VStack>
+      )}
+    />
   );
 };
 
