db27c07f455ed169ac9c43720f7ca95797860185
Simona Ilcheva <sdmileva@gmail.com>
2023-06-07 11:42:23 +0300

    Add(#7): add Calories tab

---
+++ dare2fit-companion-app/tabs/Calories/Calories.tsx
@@ -0,0 +1,11 @@
+import { Center, Text, VStack } from "native-base";
+
+const Calories = () => {
+  return (
+    <Center w="100%" h="100%" p={2}>
+      <Text>Calories tab</Text>
+    </Center>
+  );
+};
+
+export default Calories;
--- dare2fit-companion-app/tabs/TabNavigation.tsx
+++ dare2fit-companion-app/tabs/TabNavigation.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 
 import Activity from "./Activity/Activity";
+import Calories from "./Calories/Calories";
 import Profile from "./Profile/Profile";
 
 const Tab = createBottomTabNavigator();
@@ -12,6 +13,7 @@ const TabNavigation = () => {
       <Tab.Navigator>
         <Tab.Screen name="Profile" component={Profile} />
         <Tab.Screen name="Activity" component={Activity} />
+        <Tab.Screen name="Calories" component={Calories} />
       </Tab.Navigator>
     </NavigationContainer>
   );
